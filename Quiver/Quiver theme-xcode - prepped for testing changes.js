30cb6c5f639aee8e3736a7228561c4bf2b958483
Rami <rami_s@hotmail.com>
2019-12-30 16:20:32 +0100

    quiver and xcode color updates

--- Quiver/Quiver theme-xcode - prepped for testing changes.js
+++ Quiver/Quiver theme-xcode - prepped for testing changes.js
@@ -1,3 +1,4 @@
+Old and Notes
 ace.define("ace/theme/xcode",["require","exports","module","ace/lib/dom"],function(e,t,n){t.isDark=!1,t.cssClass="ace-xcode",t.cssText="
 .ace-xcode .ace_gutter {background: #282A36;color: #565A6D} <done>
 .ace-xcode .ace_print-margin {width: 1px;background: #30DC23} <testing as green>
@@ -21,7 +22,7 @@ removed -> .ace-xcode .ace_marker-layer .ace_bracket {margin: -1px 0 0 -1px;bord
 .ace-xcode .ace_constant.ace_numeric {font-weight: 600;color: #59C29C} <done>
 
 .ace-xcode .ace_entity.ace_other.ace_attribute-name {color: #30DC23} <testing as green - ok not sure where>
-.ace-xcode .ace_support.ace_constant {color: #59C29C} <string green ok - css inline-block, solid, table-cell>
+.ace-xcode .ace_support.ace_constant {color: #47B989} <string green ok - css inline-block, solid, table-cell>
 .ace-xcode .ace_support.ace_function {color: #2B74CC} <done - could breakup more - subscript, print, (return), (func calls)>
 added -> .ace-xcode .ace_punctuation.ace_operator {color: #C1C1C1} < does nothing so far??)>
 added -> .ace-xcode .ace_paren {color: #C1C1C1} < done! ()>
@@ -29,10 +30,43 @@ added -> .ace-xcode .ace_function.ace_buildin {color: #30DC23} <testing as green
 added -> .ace-xcode .ace_support {color: #30DC23} <testing as green - ok not sure where>
 added -> .ace-xcode .ace_definition,.ace-xcode .ace_type {color: #30DC23} <testing as green - ok not sure where>
 
-.ace-xcode .ace_fold {background-color: #565A6D;border-color: #7F7F7F} <done - folding icon>
+.ace-xcode .ace_fold {background-color: #565A6D;border-color: #7F7F7F} <done - folding icon (border-color-> outer border)>#FDAF79
 .ace-xcode .ace_entity.ace_name.ace_tag,.ace-xcode .ace_support.ace_class,.ace-xcode .ace_support.ace_type {color: #EE87C5} <fine pink in CSS -> attributes>
 .ace-xcode .ace_storage {color: #2B74CC} <done - Int, String>
-.ace-xcode .ace_string {color: #59C29C} <done>
+.ace-xcode .ace_string {color: #47B989} <done>
 .ace-xcode .ace_string.ace_regexp {color: #30DC23;} <testing as green - not sure where>
-.ace-xcode .ace_comment {font-style: italic;color: #7F7F7F} <done>
+.ace-xcode .ace_comment {font-size: 12px;font-style: italic;color: #7F7F7F} <done>
+.ace-xcode .ace_indent-guide {background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==) right repeat-y}";var r=e("../lib/dom");r.importCssString(t.cssText,t.cssClass)})
+
+
+
+
+Current
+ace.define("ace/theme/xcode",["require","exports","module","ace/lib/dom"],function(e,t,n){t.isDark=!1,t.cssClass="ace-xcode",t.cssText="
+.ace-xcode .ace_gutter {background: #282A36;color: #565A6D}
+.ace-xcode .ace_print-margin {width: 1px;background: #30DC23}
+.ace-xcode {background-color: #282A36;color: #CDCCCC}
+.ace-xcode .ace_cursor {color: #FFFFFF}.ace-xcode .ace_marker-layer .ace_selection {background: #424556}
+.ace-xcode.ace_multiselect .ace_selection.ace_start {box-shadow: 0 0 3px 0px #FFCC68;}
+.ace-xcode .ace_marker-layer .ace_step {background: #30DC23}
+.ace-xcode .ace_marker-layer .ace_bracket {margin: -1px 0 0 -1px;border: 1px solid #FFCC68}
+.ace-xcode .ace_marker-layer .ace_active-line {background: rgba(0, 0, 0, 0.071)}
+.ace-xcode .ace_marker-layer .ace_selected-word {border: 1px solid #FFCC68}
+.ace-xcode .ace_constant.ace_language {color: #9B76CE}
+.ace-xcode .ace_keyword {color: #9B76CE}
+.ace-xcode .ace_keyword.ace_operator {color: #C1C1C1;}
+.ace-xcode .ace_meta,.ace-xcode .ace_variable.ace_language {color: #C1C1C1}
+.ace-xcode .ace_invisible {color: #343744}
+.ace-xcode .ace_constant.ace_character,.ace-xcode .ace_constant.ace_other {color: #275A5E}
+.ace-xcode .ace_constant.ace_numeric {font-weight: 600;color: #47B989}
+.ace-xcode .ace_entity.ace_other.ace_attribute-name {color: #30DC23}
+.ace-xcode .ace_support.ace_constant {color: #47B989}
+.ace-xcode .ace_support.ace_function {color: #2B74CC}
+
+.ace-xcode .ace_fold {background-color: #565A6D;border-color: #7F7F7F}
+.ace-xcode .ace_entity.ace_name.ace_tag,.ace-xcode .ace_support.ace_class,.ace-xcode .ace_support.ace_type {color: #A4C9FF} <light blue in CSS -> attributes>
+.ace-xcode .ace_storage {color: #2B74CC} <done - Int, String>
+.ace-xcode .ace_string {color: #47B989} <done>
+.ace-xcode .ace_string.ace_regexp {color: #30DC23;} <testing as green - not sure where>
+.ace-xcode .ace_comment {font-size: 12px;font-style: italic;color: #7F7F7F}  <done>
 .ace-xcode .ace_indent-guide {background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAAE0lEQVQImWP4////f4bLly//BwAmVgd1/w11/gAAAABJRU5ErkJggg==) right repeat-y}";var r=e("../lib/dom");r.importCssString(t.cssText,t.cssClass)})
